cd19db047f82ec34b754fd83edae13bd5b6dd924
Andrew Sherman <andrew@ohair-sherman.com>
2017-03-11 15:50:08 -0800

    using namespace std

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 
-std::string foo( std::string str)
+std::string foo(string str)
 {
-    std::printf("foo:str %s is at %x", str, &str);
+   printf("foo:str %s is at %x", str, &str);
 }
 
-std::string bar( std::string* str)
+std::string bar(string* str)
 {
-    std::printf("bar:str %s is at %x", *str, str);
+   printf("bar:str %s is at %x", *str, str);
 }
 
 int main() {
-    std::string hello1 = "Hello, World!";
-    std::cout << hello1 << std::endl;
+    using namespace std;
+    
+   string hello1 = "Hello, World!";
+   cout << hello1 <<endl;
 
-    std::printf("hello1 is at %x", &hello1);
-    std::printf("main:str %s is at %x", hello1, &hello1);
+   printf("hello1 is at %x", &hello1);
+   printf("main:str %s is at %x", hello1, &hello1);
     foo(hello1);
     bar(&hello1);
 
