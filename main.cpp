b154d8dd29316c9209f5e372b3451355323dc8cb
Andrew Sherman <andrew@ohair-sherman.com>
2017-03-11 15:57:50 -0800

    declare functions as void and it works, but...

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 
 using namespace std;
 
-std::string foo(string str)
+void foo(string str)
 {
     cout << "foo:str '" << str << "' is at " << &str << endl;
 }
 
-std::string bar(string* str)
+void bar(string* str)
 {
     cout << "bar:str '" << *str << "' is at " << str << endl;
 }
@@ -19,8 +19,9 @@ int main() {
    cout << hello1 <<endl;
 
     cout << "main:str '" << hello1 << "' is at " << &hello1 << endl;
-    foo(hello1);
     bar(&hello1);
+    foo(hello1);
+
 
     return 0;
 }
