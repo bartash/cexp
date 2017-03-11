5d002a8dd8127e4e68740df81d1547c60f36092e
Andrew Sherman <andrew@ohair-sherman.com>
2017-03-11 15:48:50 -0800

    why I must not use printf

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 
+std::string foo( std::string str)
+{
+    std::printf("foo:str %s is at %x", str, &str);
+}
+
+std::string bar( std::string* str)
+{
+    std::printf("bar:str %s is at %x", *str, str);
+}
+
 int main() {
-    std::string string = "Hello, World!";
-    std::cout << string << std::endl;
+    std::string hello1 = "Hello, World!";
+    std::cout << hello1 << std::endl;
+
+    std::printf("hello1 is at %x", &hello1);
+    std::printf("main:str %s is at %x", hello1, &hello1);
+    foo(hello1);
+    bar(&hello1);
+
     return 0;
 }
+
 
